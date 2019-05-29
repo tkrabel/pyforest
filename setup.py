80793313610d547e13baf11ca41e162a6fad02c7
tkrabel <tobiaskrabel@gmx.de>
2019-05-29 16:23:30 +0200

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'pyforest',
+  packages = ['pyforest'],
+  version = '0.0.0',
+  license='',
+  description = '',
+  author = 'Tobias Krabel, Florian Wetschoreck',
+  author_email = 'tobias@edaviz.com, florian@edaviz.com',
+  url = 'https://github.com/tkrabel/pyforest',
+  download_url = '',
+  keywords = [],
+  install_requires=[],
+  classifiers=[],
+)
